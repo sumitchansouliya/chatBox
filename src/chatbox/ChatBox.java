5cf499b81c0a6b3d32350167bb09991a2dbb4cc9
sumitchansouliya <sumitchansouliya@gmail.com>
2015-03-18 17:51:31 +0530

    new commit

---
+++ src/chatbox/ChatBox.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatbox;
+
+/**
+ *
+ * @author sumit
+ */
+public class ChatBox {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
